d614c14088763fe6b40e9d0d912e2d478286d54a
Jamol <karshievjamol19@gmail.com>
2022-01-24 23:05:32 +0100

    Update index.jsx
    hm

--- src/root/context/index.jsx
+++ src/root/context/index.jsx
@@ -9,7 +9,9 @@ export const Context =(props)=>{
     const [data, setData]= useState(movie);
   return(
      <MovieContext.Provider value={[data, setData]}>
+        
          {props.children}
+         
      </MovieContext.Provider>
    );
 };
